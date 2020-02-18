5fed1783954f587aa6412b67bfea6e185a20211f
Oleh Palamarchuk <60472851+palamarchuk004@users.noreply.github.com>
2020-02-18 15:42:30 +0200

    Delete DZ1.2.py

--- DZ1.2.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name?   ")
-age = input("How old are you?   ")
-place = input("Where do you live?   ")
-print("Hello", name,
-      "Your age is", age,
-      "You live in", place)
