0dc3e6179920248fe4bb9bacd3041e0c4c25fc9b
Oleh Palamarchuk <60472851+palamarchuk004@users.noreply.github.com>
2020-02-20 13:59:11 +0200

    Add files via upload

---
+++ HW6.3.py
@@ -0,0 +1,5 @@
+          # No yelling!
+
+def filter_words(st):
+    return st.capitalize()
+print(filter_words('HELLO world!'))
---
+++ HW6.4.py
@@ -0,0 +1,10 @@
+          # Unfinished Loop - Bug Fixing #1
+
+def create_array(n):
+    res = []
+    i = 1
+    while n > len(res):
+        res.append(i)
+        i += 1
+    return res
+print(create_array(5))
